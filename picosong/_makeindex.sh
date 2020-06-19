7fbae4a65763e03a09c19d48cf811b268528c0c8
Christian Valencia <31971056+SaintOcean@users.noreply.github.com>
2020-06-19 14:07:34 -0700

    upld & update

--- picosong/_makeindex.sh
+++ picosong/_makeindex.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ls | egrep '(mp3)' | perl -e 'print "<html><body><h1>Picosong</h1><ul>"; while(<>) { chop $_; print "<p><a href=\"./$_\">/$_</a></p>";} print "</ul></body></html>"' > index.html
+ls | egrep '(mp3|wav)' | perl -e 'print "<html><body><h1>Picosong</h1><ul>"; while(<>) { chop $_; print "<p><a href=\"./$_\">/$_</a></p>";} print "</ul></body></html>"' > index.html
