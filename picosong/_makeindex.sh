c2c760dc3753ad9ae0f986b52ffbef1a4a114160
Christian Valencia <31971056+SaintOcean@users.noreply.github.com>
2025-11-17 18:42:18 -0800

    upload

--- picosong/_makeindex.sh
+++ picosong/_makeindex.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-
 ls | egrep '(mp3|mp4|wav)' | perl -e 'print "<html><body><h1>Picosong</h1><ul>"; while(<>) { chop $_; print "<p><a href=\"./$_\">/$_</a></p>";} print "</ul></body></html>"' > index.html
